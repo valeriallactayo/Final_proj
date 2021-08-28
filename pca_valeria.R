c9b2d33bc3903d25c8aa86c5038f68b9c04342ef
Valeria <valeria.llactayo@unmsm.edu.pe>
2021-08-28 09:01:20 -0500

    Create pca_valeria.R

---
+++ pca_valeria.R
@@ -0,0 +1,64 @@
+rm(list = ls())
+library(psych)
+library(PerformanceAnalytics)
+library(GGally)
+library(lattice)
+library(cptcity) #paleta de colores
+pacman :: p_load(tidyverse)
+
+fires <- read_csv("forestfires.csv") %>% 
+  print()
+
+fires %>% select(-c("X", "Y", "day"))
+
+fires_pca <- select(fires, 5:13) %>% 
+  print()
+
+#describiendo los datos
+describe(fires_pca)
+
+#esttandarizando
+fires_pca_esc <- scale(fires_pca) %>% as_tibble() %>% 
+  print()
+
+#matriz de correlación
+correlation <- cor(fires_pca_esc)
+
+#matriz de varianza-covarianza
+matrix_cov <- cov(fires_pca_esc)
+
+#suma de la diagonal o "varianza total"
+diag(matrix_cov) %>% sum() 
+
+chart.Correlation(fires_pca_esc, histogram = T, pch = 20)
+
+levelplot(
+  correlation,
+  col.regions = 
+    cpt(
+      pal = "cb_div_RdBu_11", n = 100
+    )
+)
+
+#calculando eigenvectors y eigenvalues
+
+eigen <- eigen(matrix_cov) %>% print()
+
+#calculando el pca
+library(ade4)
+pca <- dudi.pca(fires_pca_esc, 
+                scale = F, 
+                scannf = F,
+                nf = ncol(fires_pca_esc)
+)
+summary(pca)
+
+pca$eig
+
+sum(pca$eig)
+
+#autovectores
+pca$c1
+
+library(factoextra)
+fviz_eig(pca,addlabels = T)
